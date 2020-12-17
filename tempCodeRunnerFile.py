1996854018ccef6f1ef0c5a8461aa5332f1739cd
sa03576 <31242894+sa03576@users.noreply.github.com>
2020-12-17 05:46:08 +0500

    Revert "Revert "EVERYTHING DONEEEEE""
    This reverts commit f93a83e70de9f93eda38a8204bdc8ddb05da1e16.

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-
-    # for u in range(len(the_path)-1):
-    #     y = the_path[u][0]*zoom + int(zoom/2)
-    #     x = the_path[u][1]*zoom + int(zoom/2)
-    #     y1 = the_path[u+1][0]*zoom + int(zoom/2)
-    #     x1 = the_path[u+1][1]*zoom + int(zoom/2)
-    #     draw.line((x,y,x1,y1), fill=(255, 0,0), width=5)
-    # draw.rectangle((0, 0, zoom * len(a[0]), zoom * len(a)), outline=(0,255,0), width=2)
-    # images.append(im)
+in(10)
